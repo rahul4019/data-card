489ed8be757e44650e36ab3dd9536ad897d7c9dd
rahul4019 <rahulkashyap4019@gmail.com>
2023-01-22 19:33:22 +0530

    Implement functionality of view details button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import ListCard from "./ListCard";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen p-4 bg-blue-50">
+    <div className="flex flex-col items-center h-screen w-screen p-4 bg-blue-50">
       <ListCard />
-      <ListCard />
-      <ListCard />
+      {/* <ListCard />
+      <ListCard /> */}
      </div>
   );
 };
--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useState } from "react";
+
+import DetailCard from "./DetailCard";
 
 const ListCard = () => {
+  const [expandCard, setExpandCard] = useState(false);
+
+  const handleBtn = () => {
+    setExpandCard(!expandCard);
+  };
+
+  const expandedCardStyle =
+    "flex flex-col bg-white items-center justify-center border rounded-lg shadow-md p-2 my-5 w-full max-w-7xl md:flex-row md:justify-around md:pl-4 md:h-screen ease-in-out duration-300";
+
+  const shrinkCardStyle =
+    "flex flex-col bg-white items-center justify-center border rounded-lg shadow-md p-2 my-5 w-full max-w-7xl md:flex-row md:justify-around md:pl-4 md:h-36 ease-in-out duration-300";
+
   return (
-    <div className="flex flex-col bg-white items-center justify-center border rounded-lg shadow-md p-2 my-5 md:flex-row md:justify-around md:pl-4 md:h-36">
+    <div className={expandCard ? expandedCardStyle : shrinkCardStyle}>
       <div className="my-2 text-xl md:text-lg">Hellomet</div>
       <div className="flex flex-col w-full md:flex-row md:justify-around ">
         <div className="flex justify-evenly my-2 md:flex-col">
@@ -19,10 +34,14 @@ const ListCard = () => {
         </div>
       </div>
       <div className="flex w-full justify-center my-2 md:w-1/3 md:justify-end">
-        <button className="w-1/2 bg-red-500 text-white p-2 rounded-full hover:bg-red-700 md:w-2/3">
-          View Details
+        <button
+          className="w-1/2 bg-red-500 text-white p-2 rounded-full hover:bg-red-600 md:w-2/3"
+          onClick={() => handleBtn()}
+        >
+          {expandCard ? "Hide Details" : "View Details"}
         </button>
       </div>
+      {expandCard && <DetailCard />}
     </div>
   );
 };
