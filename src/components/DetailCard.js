b3f3b63f4f6cb73cb9b22a88bc2cd6f9f6f5f15c
rahul4019 <rahulkashyap4019@gmail.com>
2023-01-22 20:44:56 +0530

    Add detailCard component and style it

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const DetailCard = () => {
   return (
-    <div>DetailCard</div>
-  )
-}
+    <div className="flex flex-col p-2 border-2 rounded-lg shadow-md m-4">
+      <div className="flex flex-col">
+        <div className="font-bold">Description</div>
+        <p className="font-normal">
+          Providing service of domestic flight booking @ lowest price guaranted
+          and also for railway e ticket booking, also offering international
+          flight tickets. giving services to our customers since 1995. now going
+          to start hajj and umrah very soon for our valuable customers it will
+          be also @ very affordable prices.
+        </p>
+      </div>
 
-export default DetailCard
+      <div className="grid grid-cols-1 gap-4  md:grid-cols-2">
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">Contact Person</div>
+          <p>Imran Dola</p>
+        </div>
+        <div className="flex justify-between md:flex-col ">
+          <div className="font-bold">Address</div>
+          <p className="text-right md:text-left">
+            15 Kashmani Shopping Centre Unn Surat Gujarat 395003
+          </p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">Designation</div>
+          <p>Proprietor</p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">State</div>
+          <p>Gujarat</p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">Emails</div>
+          <p>helloservices1@gmail.com</p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">City</div>
+          <p>Surat</p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">Phones</div>
+          <p>9712500500</p>
+        </div>
+        <div className="flex justify-between md:flex-col">
+          <div className="font-bold">Country</div>
+          <p>India</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailCard;
--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -10,38 +10,34 @@ const ListCard = () => {
     setExpandCard(!expandCard);
   };
 
-  const expandedCardStyle =
-    "flex flex-col bg-white items-center justify-center border rounded-lg shadow-md p-2 my-5 w-full max-w-7xl md:flex-row md:justify-around md:pl-4 md:h-screen ease-in-out duration-300";
-
-  const shrinkCardStyle =
-    "flex flex-col bg-white items-center justify-center border rounded-lg shadow-md p-2 my-5 w-full max-w-7xl md:flex-row md:justify-around md:pl-4 md:h-36 ease-in-out duration-300";
-
   return (
-    <div className={expandCard ? expandedCardStyle : shrinkCardStyle}>
-      <div className="my-2 text-xl md:text-lg">Hellomet</div>
-      <div className="flex flex-col w-full md:flex-row md:justify-around ">
-        <div className="flex justify-evenly my-2 md:flex-col">
-          <div className="font-bold text-xl md:text-lg">CONTACT</div>
-          <p className="text-xl md:text-lg">Swapnil Warng</p>
+    <div className="flex flex-col w-full max-w-7xl border rounded-lg bg-white shadow-md ease-in-out duration-300 p-2">
+      <div className="flex flex-col items-center justify-center my-5 w-full md:flex-row md:justify-around md:pl-4 md:h-36 ">
+        <div className="my-2 text-xl md:text-lg">Hellomet</div>
+        <div className="flex flex-col w-full md:flex-row md:justify-around ">
+          <div className="flex justify-evenly my-2 md:flex-col">
+            <div className="font-bold text-xl md:text-lg">CONTACT</div>
+            <p className="text-xl md:text-lg">Swapnil Warng</p>
+          </div>
+          <div className="flex justify-evenly my-2 md:flex-col">
+            <div className="font-bold text-xl md:text-lg">CITY</div>
+            <p className="text-xl md:text-lg">Mumbai</p>
+          </div>
+          <div className="flex justify-evenly my-2 md:flex-col ">
+            <div className="font-bold text-xl md:text-lg">STATE</div>
+            <p className="text-xl md:text-lg">Maharastra</p>
+          </div>
         </div>
-        <div className="flex justify-evenly my-2 md:flex-col">
-          <div className="font-bold text-xl md:text-lg">CITY</div>
-          <p className="text-xl md:text-lg">Mumbai</p>
-        </div>
-        <div className="flex justify-evenly my-2 md:flex-col ">
-          <div className="font-bold text-xl md:text-lg">STATE</div>
-          <p className="text-xl md:text-lg">Maharastra</p>
+        <div className="flex w-full justify-center my-2 md:w-1/3 md:justify-end">
+          <button
+            className="w-1/2 bg-red-500 text-white p-2 rounded-full hover:bg-red-600 md:w-2/3"
+            onClick={() => handleBtn()}
+          >
+            {expandCard ? "Hide Details" : "View Details"}
+          </button>
         </div>
       </div>
-      <div className="flex w-full justify-center my-2 md:w-1/3 md:justify-end">
-        <button
-          className="w-1/2 bg-red-500 text-white p-2 rounded-full hover:bg-red-600 md:w-2/3"
-          onClick={() => handleBtn()}
-        >
-          {expandCard ? "Hide Details" : "View Details"}
-        </button>
-      </div>
-      {expandCard && <DetailCard />}
+      <div>{expandCard && <DetailCard />}</div>
     </div>
   );
 };
